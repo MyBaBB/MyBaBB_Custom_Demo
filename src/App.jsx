354418f5f1887969083697b86a55b9628ed3f936
Brett Baker <GitBaBB@MyBaBB.com>
2024-03-29 09:01:54 -0700

    I have all of my google fonts up to date on here and programmed into input.css and synce with google fonts so I can easily look up font styles and simply add a classname of that font and it will be automatic.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
-
+import "./App.css";
 const App = () => {
-  return <div>Test</div>;
+  return <div className="Yellowtail">Test</div>;
 };
 
 export default App;
