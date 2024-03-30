e5d5e9171c7fefafa0d035e584a5f53b39a77909
Brett Baker <GitBaBB@MyBaBB.com>
2024-03-30 04:42:49 -0700

    added the font size hack of 62.5 percent so that REM is porpotioned to px for figma translation

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
 import "./App.css";
+import Header from "./Components/HeaderFolder/Header";
 const App = () => {
-  return <div className="Yellowtail">Test</div>;
+
+  return <div className="min-h-screen bg-varMEDSPACE dark:bg-black dark:text-white">
+          <Header />
+         </div>
 };
 
 export default App;
